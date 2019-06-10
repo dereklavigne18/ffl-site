0a37fa7a5e068c4071fff039ab2203f503a1b811
Derek Lavigne <dlavigne@wayfair.com>
2019-06-10 00:11:52 -0400

    Start of redis installation script

---
+++ scripts/install_redis_server.sh
@@ -0,0 +1,14 @@
+# Download redis and unpack
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+
+# Build the binary
+cd redis-stable
+make
+
+# Add Redis to the machine's path
+sudo make install
+
+# Setup firewall rule to protect Redis
+
+# Add authentication
