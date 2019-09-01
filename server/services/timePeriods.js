99fb488a06cbea4a514bbbf79aab3c7bd6b47343
dlavigne18 <derek.lavigne@comcast.net>
2019-09-01 17:33:42 -0400

    Time period calculations are fixed

--- server/services/timePeriods.js
+++ server/services/timePeriods.js
@@ -5,17 +5,35 @@ const WEDNESDAY = 3;
 const LAST_WEEK_OF_SEASON = 13;
 const FIRST_SEASON = 2018;
 
-function firstWedInSeptemberCurrentYear() {
-  const currentYear = getCurrentSeason();
-  const dayOfTheMonth = range(1, 8).find(
-    day => new Date(currentYear, SEPTEMBER, day).getDay() === WEDNESDAY,
-  );
+function dayAfter(date) {
+  const nextDay = new Date(date);
+  nextDay.setDate(nextDay.getDate() + 1);
 
-  return new Date(currentYear, SEPTEMBER, dayOfTheMonth);
+  return nextDay;
+}
+
+function firstWedAfterDate(date) {
+  let daysCheckedCount = 0;
+  const dateToCheck = date;
+
+  while (daysCheckedCount < 8) {
+    if (dateToCheck.getDay() === WEDNESDAY) {
+      return dateToCheck;
+    }
+
+    dateToCheck.setDate(dayAfter(date).getDate());
+    daysCheckedCount += 1;
+  }
+
+  throw new Error('Could not find a Wednesday.');
+}
+
+function firstWedInSeptember(year) {
+  return firstWedAfterDate(new Date(year, SEPTEMBER, 1));
 }
 
 function getCurrentWeek() {
-  const diff = new Date() - firstWedInSeptemberCurrentYear();
+  const diff = new Date() - firstWedInSeptember(getCurrentSeason());
   const weekDiff = Math.floor(diff / (1000 * 60 * 60 * 24 * 7));
   return weekDiff > LAST_WEEK_OF_SEASON ? LAST_WEEK_OF_SEASON : weekDiff;
 }
@@ -23,8 +41,9 @@ function getCurrentWeek() {
 function getCurrentSeason() {
   const now = new Date();
   const currentYear = now.getFullYear();
+  const seasonStartDate = firstWedInSeptember(currentYear);
 
-  return now.getMonth() < SEPTEMBER ? currentYear - 1 : currentYear;
+  return now < seasonStartDate ? currentYear - 1 : currentYear;
 }
 
 function getSeasons() {
@@ -32,9 +51,9 @@ function getSeasons() {
   return range(FIRST_SEASON, currentSeason + 1).map(season => ({
     year: season,
     weeks:
-      season === currentSeason
+      season !== currentSeason
         ? range(1, LAST_WEEK_OF_SEASON + 1)
-        : range(1, getCurrentWeek()),
+        : range(1, getCurrentWeek() + 1),
   }));
 }
 
